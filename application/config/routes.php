fcc97cdae01d73bd92ddce5bc41cd9d8fd138732
HP-PC\HP <rmoayinla@yahoo.com>
2018-06-02 23:15:34 +0100

    Added a route for login processing

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['(:any)'] = 'Welcome/view/$1';
+$route['client/login'] = 'Authentication/login';
 $route['default_controller'] = 'Welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
