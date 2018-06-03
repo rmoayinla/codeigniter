11321433f4d1106c02397d424005e92c27ab9173
HP-PC\HP <rmoayinla@yahoo.com>
2018-06-03 10:30:07 +0100

    Added functionality for 2 step signup form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,59 @@
 (function($){
-	//alert("loaded");
+	
+	$(function(){
+		
+		var signupFormValidate = $("#signup-form").validate({
+	      	rules: {
+	      		client_fullname: {
+	      			required: true,
+	      		},
+	      		client_username:{
+	      			required: true,
+	      			pattern: /^([a-zA-Z0-9])+$/,
+	      		},
+	      		client_email: {
+	      			required: true,
+	      			email: true,
+	      		}
+	      	},
+	      	messages:{
+	      		client_fullname: "Enter your full name please", 
+	      		client_username: {
+	      			required: "Username is required for accessing the dashboard",
+	      			pattern: "Invalid characters, only Alphabets and Numbers are allowed"
+	      		},
+	      		client_email: {
+	      			required: "Please enter your email address",
+	      			email: "Invalid email, please retype your email address"
+	      		}
+	      	},
+	      	errorElement: "span",
+		});
+
+		$("#signup-form .next-button").on("click", function(ev){
+			ev.preventDefault();
+			if(signupFormValidate.form()){
+			var _this, prevParent,nextParent;
+			_this = $(this);
+			stepParent = _this.parents(".form-step");
+			nextParent = stepParent.next(".form-step");
+			setTimeout( function(){nextParent.addClass("slide-in").removeClass("slide-out fade-out");}, 800);
+			stepParent.removeClass("slide-in").addClass("slide-out fade-out");
+			}
+			
+		});
+
+		$("#signup-form .prev-button").on("click", function(ev){
+			ev.preventDefault();
+			var _this, prevParent,nextParent;
+			_this = $(this);
+			stepParent = _this.parents(".form-step");
+			prevParent = stepParent.prev(".form-step");
+			setTimeout( function(){prevParent.addClass("slide-in").removeClass("slide-out fade-out");}, 800);
+			stepParent.removeClass("slide-in").addClass("slide-out fade-out");
+			
+		});
+	});
+	
+
 })(jQuery);
