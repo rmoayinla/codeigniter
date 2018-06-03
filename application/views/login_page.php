0e6a8edd3ae181d5a20b869cdc5b75a93a82e339
HP-PC\HP <rmoayinla@yahoo.com>
2018-06-03 10:16:47 +0100

    changed the name fo the login page

--- application/views/login_page.php
+++ application/views/login_page.php
@@ -17,15 +17,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					<div class="col-lg-4 mx-auto">
 						<h1>Login</h1>
 						<div class="alert alert-warning"><?php echo $this->session->flashdata('validation_errors'); ?></div>
-						<form method="POST" action="client/login">
+						<form method="POST" action="client/login" id="login-form">
 							<div class="form-group">
 							  <label for="exampleFormControlInput1">Email or Username </label>
-							  <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" name="client_email_or_username" />
+							  <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" name="client_email_or_username" required />
 							</div>
 
 							<div class="form-group">
 								  <label for="exampleFormControlInput1">Password</label>
-								  <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" name="client_password" />
+								  <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" name="client_password" required />
 							</div>
 							<input type="hidden" name="return_url" value="<?php echo current_url(); ?>" />
 							<div class="form-group">
