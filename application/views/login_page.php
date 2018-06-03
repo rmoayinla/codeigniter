46804293df0b419cf078e29985e98448f2d3c4f9
HP-PC\HP <rmoayinla@yahoo.com>
2018-06-03 11:41:21 +0100

    Now validation errors are shown only if the form have been submitted and there is an error

--- application/views/login_page.php
+++ application/views/login_page.php
@@ -10,30 +10,40 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 	<body>
 		<?php include VIEWPATH."partials/navbar.php" ?>
+		
 		<div id="container" class="container-fluid">
 
-			<main class="py-5">
+			<main class="py-5" id="main-content">
 				<div class="row">
 					<div class="col-lg-4 mx-auto">
 						<h1>Login</h1>
-						<div class="alert alert-warning"><?php echo $this->session->flashdata('validation_errors'); ?></div>
+						
+						<?php if(!empty($this->session->flashdata('validation_errors'))): ?>
+							<div class="alert alert-warning">
+								<?php echo $this->session->flashdata('validation_errors'); ?>
+							</div>
+						<?php endif; ?>
+						
 						<form method="POST" action="client/login" id="login-form">
 							<div class="form-group">
-							  <label for="exampleFormControlInput1">Email or Username </label>
-							  <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" name="client_email_or_username" required />
+							  <label for="">Email or Username </label>
+							  <input type="text" class="form-control" id="" placeholder="name@example.com" name="client_email_or_username" required />
 							</div>
 
 							<div class="form-group">
-								  <label for="exampleFormControlInput1">Password</label>
-								  <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" name="client_password" required />
+								  <label for="">Password</label>
+								  <input type="password" class="form-control" id="" placeholder="name@example.com" name="client_password" required />
 							</div>
+							<!--hidden input to set the referrer url, this is use at server side for redirection -->
 							<input type="hidden" name="return_url" value="<?php echo current_url(); ?>" />
+							
 							<div class="form-group">
-								<button type="submit" class="btn btn-primary">Submit</button>
+								<button type="submit" class="btn btn-primary">Login </button>
 							</div>
-						</form>
-					</div>
-				</div>
-			</main>
+						</form><!--#login-form -->
+					</div><!--.col-lg-4 -->
+				</div><!--.row -->
+			</main><!--#main-content closes -->
 
+<!--include the footer partial -->
 <?php include VIEWPATH."partials/footer.php" ?>
