2c003a671cd9251356969369a5caefc6aa68b0a1
HP-PC\HP <rmoayinla@yahoo.com>
2018-06-03 17:23:21 +0100

    Signup form validation and a 2-step form

--- js/script.js
+++ js/script.js
@@ -32,25 +32,44 @@
 
 		$("#signup-form .next-button").on("click", function(ev){
 			ev.preventDefault();
-			if(signupFormValidate.form()){
+			if(!signupFormValidate.form())return false;
+			
 			var _this, prevParent,nextParent;
 			_this = $(this);
 			stepParent = _this.parents(".form-step");
 			nextParent = stepParent.next(".form-step");
 			setTimeout( function(){nextParent.addClass("slide-in").removeClass("slide-out fade-out");}, 800);
 			stepParent.removeClass("slide-in").addClass("slide-out fade-out");
-			}
+			
 			
 		});
 
 		$("#signup-form .prev-button").on("click", function(ev){
 			ev.preventDefault();
-			var _this, prevParent,nextParent;
+			if(!signupFormValidate.form())return false;
+			var _this, prevParent,nextParent, stepParent;
 			_this = $(this);
 			stepParent = _this.parents(".form-step");
 			prevParent = stepParent.prev(".form-step");
 			setTimeout( function(){prevParent.addClass("slide-in").removeClass("slide-out fade-out");}, 800);
 			stepParent.removeClass("slide-in").addClass("slide-out fade-out");
+			
+		});
+
+		$("#signup-form .preview-button").on("click", function(ev){
+			ev.preventDefault();
+			var _this, prevParent,nextParent, stepParent, html;
+			_this = $(this);
+			stepParent = _this.parents(".form-step");
+			nextParent = stepParent.next(".form-step");
+			html = "<table>";
+			$.each($('#signup-form').serializeArray(), function(i, field) {
+			    html += "<tr><td>" + field.name + "</td><td>" + field.value + "</td></tr>";
+			});
+			html += "</table>";
+			setTimeout( function(){nextParent.addClass("slide-in").removeClass("slide-out fade-out");}, 800);
+			stepParent.removeClass("slide-in").addClass("slide-out fade-out");
+			nextParent.find(".form-preview").html(html);
 			
 		});
 	});
