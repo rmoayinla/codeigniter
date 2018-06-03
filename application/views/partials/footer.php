cf3ee86504d2484d38c92670db4b81ba734d80ff
HP-PC\HP <rmoayinla@yahoo.com>
2018-06-03 10:30:40 +0100

    Footer partial for loading scripts

---
+++ application/views/partials/footer.php
@@ -0,0 +1,22 @@
+			<footer class="footer">
+				<p class="text-center"> Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?>
+				</p>
+			</footer>
+
+		</div><!--#container closes -->
+
+		<!--jquery script -->
+		<script src="<?php echo base_url()."jquery/jquery.min.js"?>"></script>
+
+		<!-- bootstrap js -->
+		<script src="<?php echo base_url()."jquery-validation/dist/jquery.validate.min.js"?>"></script>
+		<script src="<?php echo base_url()."jquery-validation/dist/additional-methods.min.js"?>"></script>
+		
+		<!-- bootstrap js -->
+		<script src="<?php echo base_url()."bootstrap/dist/js/bootstrap.min.js"?>"></script>
+
+		<!-- app custom script js -->
+		<script src="<?php echo base_url()."js/script.js"?>"></script>
+
+	</body>
+</html>
