deb2b35cee6574ea6a25ad2a985b5345bd975f45
HP-PC\HP <rmoayinla@yahoo.com>
2018-06-03 11:29:49 +0100

    Added a logo for navbar brand

--- application/views/partials/navbar.php
+++ application/views/partials/navbar.php
@@ -1,5 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark sticky-top bg-secondary">
-  <a class="navbar-brand" href="<?php echo site_url(); ?>">Navbar</a>
+  <a class="navbar-brand" href="<?php echo site_url(); ?>">
+    <img src="<?php echo site_url()."images/logo.png"?>" class="navbar-brand-logo" alt="">
+  </a>
   
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
